4c8c47e72b2e3ebd20c3f900d8441e3e275b5a6d
Deepti Kandwal <c_dkandwal@costco.com>
2023-12-15 12:33:01 +0530

    Optimisation of code done.

---
+++ presentation/src/main/java/com/disneyLand/ui/view/screens/details/DisneyDetailMviContract.kt
@@ -0,0 +1,26 @@
+package com.disneyLand.ui.view.screens.details
+
+import com.disneyLand.base.BaseMviContract
+import com.disneyLand.base.SideEffect
+import com.disneyLand.base.ViewIntent
+import com.disneyLand.base.ViewState
+import com.disneyLand.model.Actor
+
+interface DisneyDetailMviContract :
+    BaseMviContract<DisneyDetailMviContract.DisneyDetailScreenViewState, DisneyDetailMviContract.DisneyDetailScreenIntent, DisneyDetailMviContract.DisneyDetailScreenSideEffect> {
+    sealed class DisneyDetailScreenViewState : ViewState {
+        object LOADING : DisneyDetailScreenViewState()
+        data class SUCCESS(val data: Actor) : DisneyDetailScreenViewState()
+        data class ERROR(val error: String) : DisneyDetailScreenViewState()
+    }
+
+    sealed class DisneyDetailScreenIntent : ViewIntent {
+        class FetchCharacterById(val id: String) : DisneyDetailScreenIntent()
+        object NavigateUp : DisneyDetailScreenIntent()
+    }
+
+    sealed class DisneyDetailScreenSideEffect : SideEffect {
+        object NavigateUp : DisneyDetailScreenSideEffect()
+    }
+
+}
