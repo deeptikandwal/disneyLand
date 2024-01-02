1d40cfaed52d4cbafe1589da4d5bc91415336e98
Deepti Kandwal <c_dkandwal@costco.com>
2024-01-02 15:28:03 +0530

    commit 6:review feedback incorporated.

--- data/src/test/java/com/disneyLand/source/ActorMapperTest.kt
+++ data/src/test/java/com/disneyLand/source/ActorMapperTest.kt
@@ -1,5 +1,6 @@
 package com.disneyLand.source
 
+import com.disneyLand.dto.DisneyOriginalActor
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -14,9 +15,15 @@ class ActorMapperTest {
     }
 
     @Test
-    fun `map to actor successfully Given DisneyActorDto When mapToDisneyActor method is called Then verify name is same`() {
-        val actor = actorMapper.mapToDisneyActor(FakeDisneyActor.getSuccessfulDisneyActorDTO())
+    fun `map to actor successfully Given DisneyActor When mapToDisneyActor method is called Then verify name is same`() {
+        val actor = actorMapper.mapToDisneyActor(FakeDisneyActor.successfulDisneyActor)
         Assert.assertEquals(NAME, actor.name)
+    }
+
+    @Test
+    fun `map to actor successfully Given DisneyActor data is null When mapToDisneyActor method is called Then verify name is same`() {
+        val actor = actorMapper.mapToDisneyActor(DisneyOriginalActor(data = null))
+        Assert.assertEquals("", actor.name)
     }
 
     private companion object {
