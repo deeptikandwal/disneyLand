45d5c621359af64a71f0d374bb76a49e63851102
Deepti Kandwal <c_dkandwal@costco.com>
2024-02-11 23:06:59 +0530

    sealed class updated.

--- domain/src/main/java/com/disneyLand/Result.kt
+++ domain/src/main/java/com/disneyLand/Result.kt
@@ -2,5 +2,5 @@ package com.disneyLand
 
 sealed interface Result<out T> {
     data class Success<out T>(val value: T) : Result<T>
-    data class Error<T>(val error: Throwable) : Result<T>
+    data class Error(val error: Throwable) : Result<Nothing>
 }
